2557e9500a96d308425ef3be376de46ea91bec3b
Elizabeth Mieczkowski <eam356@cornell.edu>
2019-11-05 16:17:23 -0500

    all text for profile page added

--- carriage-user-ios/carriage-user-ios/ViewControllers/UserInputForm.swift
+++ carriage-user-ios/carriage-user-ios/ViewControllers/UserInputForm.swift
@@ -7,163 +7,219 @@
 import UIKit
 import SnapKit
 
-var firstNameInput: String! = ""
-var lastNameInput: String! = ""
-var timeInput: String! = ""
-var locationInput: String! = ""
-
 class UserInputForm: UIViewController {
     
-    var userFirstNameField: UITextField!
-    var userLastNameField: UITextField!
-    var userPickupTimeField: UITextField!
-    var userPickupLocationField: UITextField!
+    var whiteBox1: UILabel!
+    var whiteBox2: UILabel!
+    var whiteBox3: UILabel!
+    var whiteBox4: UILabel!
     
-    var submitButton: UIButton!
+    var yourProfileLabel: UILabel!
+    var nameLabel: UILabel!
+    var joinedLabel: UILabel!
+    
+    var accountInfoLabel: UILabel!
+    var emailLabel: UILabel!
+    var phoneLabel: UILabel!
+    
+    var personalInfoLabel: UILabel!
+    var pronounsLabel: UILabel!
+    var accessLabel: UILabel!
     
     var newUser: UserInputModel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.navigationItem.title = "User Input Form"
+        self.navigationController?.navigationBar.barTintColor =  UIColor(displayP3Red: 1.0, green: 1.0, blue: 1.0, alpha: 0.9)
+        self.view.backgroundColor = UIColor(displayP3Red: 1.0, green: 1.0, blue: 1.0, alpha: 0.9)
+    
+        createBackground()
         
-        newUser = UserInputModel(userFirstName: "", userLastName: "", pickUpTime: "", pickUpLocation: "")
+        yourProfileLabel = UILabel()
+        yourProfileLabel.translatesAutoresizingMaskIntoConstraints = false
+        yourProfileLabel.text = "Your Profile"
+        yourProfileLabel.textColor = .black
+        yourProfileLabel.font = UIFont(name: "SFProDisplay-Bold", size: 34)
+        self.view.addSubview(yourProfileLabel)
         
-        userFirstNameField = UITextField()
-        userFirstNameField.translatesAutoresizingMaskIntoConstraints = false
-        userFirstNameField.placeholder = "First Name"
-        userFirstNameField.textColor = .black
-        userFirstNameField.addTarget(self, action: #selector(firstNameFieldDidChange(textField:)), for: .editingChanged)
-        view.addSubview(userFirstNameField)
+        nameLabel = UILabel()
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        nameLabel.text = "Aiden Kim" //TODO: make name from user
+        nameLabel.textColor = .black
+        nameLabel.font = UIFont(name: "SFProDisplay-Bold", size: 22)
+        self.view.addSubview(nameLabel)
         
-        userLastNameField = UITextField()
-        userLastNameField.translatesAutoresizingMaskIntoConstraints = false
-        userLastNameField.placeholder = "Last Name"
-        userLastNameField.textColor = .black
-        userFirstNameField.addTarget(self, action: #selector(lastNameFieldDidChange), for: .editingChanged)
-        view.addSubview(userLastNameField)
+        joinedLabel = UILabel()
+        joinedLabel.translatesAutoresizingMaskIntoConstraints = false
+        joinedLabel.text = "Joined 06/2019" //TODO: make date from user
+        joinedLabel.textColor = UIColor(red: 0.60784, green: 0.60784, blue: 0.60784, alpha: 1.0)
+        joinedLabel.font = UIFont(name: "SFProDisplay-Bold", size: 14)
+        self.view.addSubview(joinedLabel)
         
-        userPickupTimeField = UITextField()
-        userPickupTimeField.translatesAutoresizingMaskIntoConstraints = false
-        userPickupTimeField.placeholder = "Pickup Time"
-        userPickupTimeField.textColor = .black
-        userFirstNameField.addTarget(self, action: #selector(timeFieldDidChange), for: .editingChanged)
-        view.addSubview(userPickupTimeField)
+        accountInfoLabel = UILabel()
+        accountInfoLabel.translatesAutoresizingMaskIntoConstraints = false
+        accountInfoLabel.text = "Account Info"
+        accountInfoLabel.textColor = .black
+        accountInfoLabel.font = UIFont(name: "SFProDisplay-Semibold", size: 20)
+        self.view.addSubview(accountInfoLabel)
         
-        userPickupLocationField = UITextField()
-        userPickupLocationField.translatesAutoresizingMaskIntoConstraints = false
-        userPickupLocationField.placeholder = "Pickup Location"
-        userPickupLocationField.textColor = .black
-        userFirstNameField.addTarget(self, action: #selector(locationFieldDidChange), for: .editingChanged)
-        view.addSubview(userPickupLocationField)
+        emailLabel = UILabel()
+        emailLabel.translatesAutoresizingMaskIntoConstraints = false
+        emailLabel.text = "yk568@cornell.edu" //TODO: make phone from user
+        emailLabel.textColor = UIColor(red: 0.2902, green: 0.2902, blue: 0.2902, alpha: 1.0)
+        emailLabel.font = UIFont(name: "SFProText-Regular", size: 16)
+        self.view.addSubview(emailLabel)
         
-        submitButton = UIButton()
-        submitButton.translatesAutoresizingMaskIntoConstraints = false
-        submitButton.setTitle("Submit", for: .normal)
-        //TODO: set up objc function to change user upon input depending on text fields
-        submitButton.addTarget(self, action: #selector(submitUserData), for: .touchUpInside)
-        submitButton.backgroundColor = .red
-        view.addSubview(submitButton)
+        phoneLabel = UILabel()
+        phoneLabel.translatesAutoresizingMaskIntoConstraints = false
+        phoneLabel.text = "+1 657-500-1311" //TODO: make phone from user
+        phoneLabel.textColor = UIColor(red: 0.2902, green: 0.2902, blue: 0.2902, alpha: 1.0)
+        phoneLabel.font = UIFont(name: "SFProText-Regular", size: 16)
+        self.view.addSubview(phoneLabel)
+        
+        personalInfoLabel = UILabel()
+        personalInfoLabel.translatesAutoresizingMaskIntoConstraints = false
+        personalInfoLabel.text = "Personal Info"
+        personalInfoLabel.textColor = .black
+        personalInfoLabel.font = UIFont(name: "SFProDisplay-Semibold", size: 20)
+        self.view.addSubview(personalInfoLabel)
+        
+        pronounsLabel = UILabel()
+        pronounsLabel.translatesAutoresizingMaskIntoConstraints = false
+        pronounsLabel.text = "He/Him/His" //TODO: make pronouns from user
+        pronounsLabel.textColor = UIColor(red: 0.2902, green: 0.2902, blue: 0.2902, alpha: 1.0)
+        pronounsLabel.font = UIFont(name: "SFProText-Regular", size: 16)
+        self.view.addSubview(pronounsLabel)
+        
+        accessLabel = UILabel()
+        accessLabel.translatesAutoresizingMaskIntoConstraints = false
+        accessLabel.text = "Accessibility"
+        accessLabel.textColor = UIColor(red: 0.2902, green: 0.2902, blue: 0.2902, alpha: 1.0)
+        accessLabel.font = UIFont(name: "SFProText-Regular", size: 16)
+        self.view.addSubview(accessLabel)
         
         setUpConstraints()
         
     }
     
+    func createBackground() {
+        
+        whiteBox1 = UILabel()
+        whiteBox1.translatesAutoresizingMaskIntoConstraints = false
+        whiteBox1.backgroundColor = .white
+        self.view.addSubview(whiteBox1)
+        
+        whiteBox2 = UILabel()
+        whiteBox2.translatesAutoresizingMaskIntoConstraints = false
+        whiteBox2.backgroundColor = .white
+        self.view.addSubview(whiteBox2)
+        
+        whiteBox3 = UILabel()
+        whiteBox3.translatesAutoresizingMaskIntoConstraints = false
+        whiteBox3.backgroundColor = .white
+        self.view.addSubview(whiteBox3)
+        
+        whiteBox4 = UILabel()
+        whiteBox4.translatesAutoresizingMaskIntoConstraints = false
+        whiteBox4.backgroundColor = .white
+        self.view.addSubview(whiteBox4)
+        
+    }
+    
     func setUpConstraints() {
         
-        let box = UIView()
-        view.addSubview(box)
-        
-        box.snp.makeConstraints { (make) -> Void in
-            make.edges.equalTo(view).inset(UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20))
+        whiteBox1.snp.makeConstraints { (make) -> Void in
+            make.height.equalTo(153)
+            make.width.equalTo(UIScreen.main.bounds.width)
+            make.top.equalTo(140)
+            make.centerX.equalTo(self.view)
         }
         
-        NSLayoutConstraint.activate([
-            userFirstNameField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            userFirstNameField.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -75),
-            userFirstNameField.heightAnchor.constraint(equalToConstant: 25),
-            userFirstNameField.widthAnchor.constraint(equalToConstant: 100)
-            ])
-        NSLayoutConstraint.activate([
-            userLastNameField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            userLastNameField.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -25),
-            userLastNameField.heightAnchor.constraint(equalToConstant: 25),
-            userLastNameField.widthAnchor.constraint(equalToConstant: 100)
-            ])
-        NSLayoutConstraint.activate([
-            userPickupTimeField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            userPickupTimeField.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 25),
-            userPickupTimeField.heightAnchor.constraint(equalToConstant: 25),
-            userPickupTimeField.widthAnchor.constraint(equalToConstant: 100)
-            ])
-        NSLayoutConstraint.activate([
-            userPickupLocationField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            userPickupLocationField.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 75),
-            userPickupLocationField.heightAnchor.constraint(equalToConstant: 25),
-            userPickupLocationField.widthAnchor.constraint(equalToConstant: 100)
-            ])
-        NSLayoutConstraint.activate([
-            submitButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            submitButton.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 125),
-            submitButton.heightAnchor.constraint(equalToConstant: 25),
-            submitButton.widthAnchor.constraint(equalToConstant: 100)
-            ])
-    }
-    
-    //TODO: abstract these functions!
-    
-    @objc func firstNameFieldDidChange(textField: UITextField) {
-        if (textField.text == textField.placeholder) {
-            //TODO error message
+        whiteBox2.snp.makeConstraints { (make) -> Void in
+            make.height.equalTo(160)
+            make.width.equalTo(UIScreen.main.bounds.width)
+            make.top.equalTo(299)
+            make.centerX.equalTo(self.view)
         }
-        else {
-            firstNameInput = textField.text!
-        }
-    }
-    
-    @objc func lastNameFieldDidChange(textField: UITextField) {
-        if (textField.text == textField.placeholder) {
-            //TODO error message
-        }
-        else {
-            lastNameInput = textField.text!
-        }
-    }
-    
-    @objc func timeFieldDidChange(textField: UITextField) {
-        if (textField.text == textField.placeholder) {
-            //TODO error message
-        }
-        else {
-            timeInput = textField.text!
-        }
-    }
-    
-    @objc func locationFieldDidChange(textField: UITextField) {
-        if (textField.text == textField.placeholder) {
-            //TODO error message
-        }
-        else {
-            locationInput = textField.text!
-        }
-    }
-    
-    @objc func submitUserData() {
         
-        let newUser = UserInputModel(userFirstName: "", userLastName: "", pickUpTime: "", pickUpLocation: "")
+        whiteBox3.snp.makeConstraints { (make) -> Void in
+            make.height.equalTo(161)
+            make.width.equalTo(UIScreen.main.bounds.width)
+            make.top.equalTo(465)
+            make.centerX.equalTo(self.view)
+        }
         
-        //UITextFields do not differentiate between nil and ""
-        if (firstNameInput == "" || lastNameInput == "" || locationInput == "" || timeInput == "") {
-            //TODO throw some error message that user has not inputed all info
+        whiteBox4.snp.makeConstraints { (make) -> Void in
+            make.bottom.equalTo(UIScreen.main.bounds.maxY)
+            make.width.equalTo(UIScreen.main.bounds.width)
+            make.top.equalTo(632)
+            make.centerX.equalTo(self.view)
         }
-            
-        else {
-            newUser.userFirstName = firstNameInput
-            newUser.userLastName = lastNameInput
-            newUser.pickUpTime = timeInput
-            newUser.pickUpLocation = locationInput
+        
+        yourProfileLabel.snp.makeConstraints { (make) -> Void in
+            make.height.equalTo(41)
+            make.leading.equalTo(16)
+            make.width.equalTo(182)
+            make.top.equalTo(91)
         }
+        
+        nameLabel.snp.makeConstraints { (make) -> Void in
+            make.centerX.equalTo(self.view)
+            make.top.equalTo(whiteBox1).offset(51)
+            make.height.equalTo(26)
+            make.width.equalTo(102)
+        }
+        
+        joinedLabel.snp.makeConstraints { (make) -> Void in
+            make.centerX.equalTo(self.view)
+            make.top.equalTo(nameLabel).offset(30)
+            make.height.equalTo(16)
+            make.width.equalTo(104)
+        }
+        
+        accountInfoLabel.snp.makeConstraints { (make) -> Void in
+            make.leading.equalTo(16)
+            make.top.equalTo(whiteBox2).offset(24)
+            make.height.equalTo(24)
+            make.width.equalTo(113)
+        }
+        
+        emailLabel.snp.makeConstraints { (make) -> Void in
+            make.leading.equalTo(56)
+            make.top.equalTo(accountInfoLabel).offset(43)
+            make.height.equalTo(19)
+            make.width.equalTo(150)
+        }
+        
+        phoneLabel.snp.makeConstraints { (make) -> Void in
+            make.leading.equalTo(56)
+            make.top.equalTo(emailLabel).offset(56)
+            make.height.equalTo(19)
+            make.width.equalTo(150)
+        }
+        
+        personalInfoLabel.snp.makeConstraints { (make) -> Void in
+            make.leading.equalTo(16)
+            make.top.equalTo(whiteBox3).offset(24)
+            make.height.equalTo(24)
+            make.width.equalTo(116)
+        }
+        
+        pronounsLabel.snp.makeConstraints { (make) -> Void in
+            make.leading.equalTo(56)
+            make.top.equalTo(personalInfoLabel).offset(43)
+            make.height.equalTo(19)
+            make.width.equalTo(150)
+        }
+        
+        accessLabel.snp.makeConstraints { (make) -> Void in
+            make.leading.equalTo(56)
+            make.top.equalTo(pronounsLabel).offset(56)
+            make.height.equalTo(19)
+            make.width.equalTo(150)
+        }
+        
     }
     
 }
